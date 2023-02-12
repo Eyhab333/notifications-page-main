0f6e48f85178b3eb4c46f872919686fd04441e2f
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-02-12 11:40:01 +0300

    js

--- main.js
+++ main.js
@@ -1,48 +1,22 @@
 //SHOW AND HIDE ON CLICK
-const variableName = document.getElementById("");
-const anotherVariable = document.getElementById("");
+const markAsRead = document.getElementById("mark-all-as-read");
+const unReadNotifications = document.querySelectorAll(".unread")
+const unReadCounter = document.querySelector(".unread-counter")
 
-variableName.addEventListener("click", () => {
-  anotherVariable.classList.remove("");
-  variableName.style.display = "none";
-})
+unReadCounter.innerText = unReadNotifications.length
 
-//FORM VALIDATION
-let userInput = document.querySelector("[name='username']")
-let ageInput = document.querySelector("[name='age']")
+unReadNotifications.forEach((notification) => {
+  notification.addEventListener("click", () => {
+    notification.classList.remove("unread")
+    const newUnReadNotifications = document.querySelectorAll(".unread")
+    unReadCounter.innerText = newUnReadNotifications.length
+  })
 
-document.forms[0].onsubmit = function (e) {
-  let userValid = false;
-  let ageValid = false;
-
-  if (userInput.value !== "" && userInput.value.length <= 10) {
-    userValid = true;
-  }
-
-  if (ageInput.value !== "") {
-    ageValid = true;
-  }
-}
-
-// Elements
-let emailField = document.getElementById("email-field")
-let emailErrorMessage = document.getElementById("email-error-message")
-let submitButton = document.getElementById("submit-button")
-
-// Function: validate user's input 
-function validateEmail () {
-  // If user's input doesn't match the symboles => display the error message
-  if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-
-    emailErrorMessage.innerHTML = "Oops! Please check your email";
-    return false;
-  }
-  // If user's input matches the symboles => don't display the error message
-  emailErrorMessage.innerHTML = "";
-  return true
-}
-
-// prevent the submit button from submitting data
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault()
+  markAsRead.addEventListener("click", () => {
+    unReadNotifications.forEach((notification) => {
+      notification.classList.remove("unread")
+    })
+    const newUnReadNotifications = document.querySelectorAll(".unread")
+    unReadCounter.innerText = newUnReadNotifications.length
+  })
 })
